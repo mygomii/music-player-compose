c8c46521431e5cadc46547bf161ed6b68eac0c02
mijeong.ko <mijeong.ko@hunet.co.kr>
2024-07-16 16:28:48 +0900

    update version code: 4

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,6 +2,6 @@ object AppConfig {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 24
     const val TARGET_SDK = 34
-    const val VERSION_CODE = 3
+    const val VERSION_CODE = 4
     const val VERSION_NAME = "1.0"
 }
